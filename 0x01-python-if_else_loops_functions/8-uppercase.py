a6faacba381ea82cd0d42ea2e7693f5cbb6276c4
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2023-10-03 12:10:42 +0000

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
     for n in str:
         if ord(n) >= 97 and ord(n) <= 122:
             n = chr(ord(n) - 32)
-            print("{}".format(n), end="")
-            print("")
+        print("{}".format(n), end="")
+    print("")
