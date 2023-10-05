ee64ff1c2890a830de1753188931d74951cd9be8
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2023-10-05 17:51:06 +0000

    updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 """This program is used to print the addition of all the arguments. """
+
 if __name__ == "__main__":
     import sys
-
-    total = 0
-    for n in range(len(sys.argv) - 1):
-        total += int(sys.argv[n + 1])
-        print("{}".format(total))
+    res = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            res += int(arg)
+    print(res)
