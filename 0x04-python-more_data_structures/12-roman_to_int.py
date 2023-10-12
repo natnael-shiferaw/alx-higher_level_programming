dd0dd0a8f299ce264c82f12fd83d6821c552bad9
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2023-10-12 01:25:28 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python3
 
 
-def to_subtract(list_num):
-    To_sub = 0
-    Max_list = max(list_num)
-
-    for i in list_num:
-        if Max_list > i:
-            To_sub += i
-
-    return (Max_list - To_sub)
-
-
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
 
     if not isinstance(roman_string, str):
         return 0
-    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_num.keys())
+    if roman_string is None:
+        return 0
 
-    Num = 0
-    last_rom = 0
-    list_num = [0]
+    TOKEN = roman_string.upper()
+    NUMERALS = {
+        "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500}
+    END = len(TOKEN) - 1
+    RESULT = 0
 
-    for char in roman_string:
-        for r_num in list_keys:
-            if r_num == char:
-                if rom_num.get(char) <= last_rom:
-                    Num += to_subtract(list_num)
-                    list_num = [rom_num.get(char)
-                else:
-                    list_num.append(rom_num.get(char))
-                    last_rom = rom_num.get(char)
+    for idx, char in enumerate(TOKEN):
+        if idx < END and NUMERALS[char] < NUMERALS[TOKEN[idx + 1]]:
+            RESULT -= NUMERALS[char]
+        else:
+            RESULT += NUMERALS[char]
 
-    Num += to_subtract(list_num)
-    return (Num)
+    return RESULT
