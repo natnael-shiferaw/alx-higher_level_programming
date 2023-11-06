f1f1fc2724d3e19a204fb341c9a3277b513ec9e7
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2023-11-06 16:39:24 +0300

    update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-
-"""THIS PYTHON IS USED TO DEFINE
-A class Rectangle THAT INHERITS
-FROM BaseGeometry."""
-
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A CLASS THAT REPRESENTS A rectangle
-    BY USING BaseGeometry."""
+    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """USED TO Intialize A NEW Rectangle.
+        """Intialize a new Rectangle.
 
         Args:
-            width (int): REPRESENTS THE WIDTH.
-            height (int): REPRESENTS THE HEIGHT.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
-
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """A FUNCTION THAT IS USED TO RETURN
-        THE area OF THE rectangle."""
-
+        """Return the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """A FUNCTION THAT IS USED TO RETURN
-        THE print() AND str() REPRESENTATION
-        OF A RECTANGLE."""
-
+        """Return the print() and str() representation of a Rectangle."""
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
