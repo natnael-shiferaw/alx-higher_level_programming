894e2a324e0bb4847ac3051d8b8a6ee8dcd5f61a
natnael <natnaelshiferaw2123@gmail.com>
2023-12-13 08:43:35 -0800

    update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- List all shows from 'hbtn_0d_tvshows' without a linked genre
-SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_show_genres
-  RIGHT JOIN tv_shows
-  ON tv_shows.id = tv_show_genres.show_id
-  WHERE tv_show_genres.show_id IS NULL
-  ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
